ec4c9dc8f56a95eb1201fc9c8d9bd5976e5bc7b6
Chris McIntosh <ChristopherM@tlgcohort.com>
2024-08-12 20:32:47 +0000

    added labs 18, and 19

---
+++ challenge19/work.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   print() - display data to std out"""
+
+def main():
+
+    name_input = input ("What is your name?")
+    day_input = input("What day of the week is it?")
+    print("Hello, " + name_input + "! Happy " + day_input + "!")
+
+main() 
